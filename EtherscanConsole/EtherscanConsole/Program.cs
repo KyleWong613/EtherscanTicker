e3486a9e0b60c2b95317c0fceccaee1460942cf1
kylewong <kylewong@alphareds.net>
2022-01-21 22:22:35 +0800

    Complete Console Application

--- EtherscanConsole/EtherscanConsole/Program.cs
+++ EtherscanConsole/EtherscanConsole/Program.cs
@@ -3,9 +3,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Timers;
-using System.Configuration;
 using System.Threading;
+using System.Configuration;
 using System.Threading.Tasks;
 using System.Net;
 using System.Net.Http;
@@ -13,35 +12,24 @@ using System.Net.Http.Headers;
 using System.IO;
 using System.Reflection;
 using Microsoft.AspNetCore.WebUtilities;
+using Newtonsoft.Json;
 
 namespace EtherscanConsole
 {
+
     class Program
     {
-        static void Main(string[] args)
+        public static void Main()
         {
-            //System.Timers.Timer 5mintimer = new System.Timers.Timer();
-            //5mintimer.Interval = 30000;
-            Thread printer = new Thread(new ThreadStart(FiveMinuteTicker));
-            printer.Start();
+            TimerCallback tc = new TimerCallback(CryptoCompare);
+            Timer timer = new Timer(CryptoCompare, null, 0, 300000); // 300000 5 minute interval
 
-            FiveMinuteTicker();
+            Console.ReadLine();
         }
 
-        static void FiveMinuteTicker()
-        {
-            while (true)
-            {
-                CryptoCompare();
-                Thread.Sleep(1000 * 60 * 5); // 5 minutes
-
-            }
-        }
-
-        static void CryptoCompare()
+        public static void CryptoCompare(object obj)
         {
             string apiendpoint = "https://min-api.cryptocompare.com/data/price";
-
 
             var connection = ConfigurationManager.ConnectionStrings["mysqlConnection"].ConnectionString;
             string token = "";
@@ -65,9 +53,21 @@ namespace EtherscanConsole
 
                     var uri = QueryHelpers.AddQueryString(apiendpoint, query);
                     tokenprice = GetTokenPrice(uri);
-                    Console.WriteLine(rdr[0].ToString() + tokenprice);
+                    var jobj = JsonConvert.DeserializeObject<TokenPrice>(tokenprice);
+                    decimal token_price = Convert.ToDecimal(jobj.USD);
 
-                    Console.WriteLine(uri);
+                    //Console.WriteLine(jobj.Response);
+                    //Console.WriteLine(rdr[0].ToString() + jobj.USD);
+
+                    //coin pair Error does not exist update price as 0
+                    if (jobj.Response == "Error")
+                    {
+                        UpdateTokenPrice(rdr[0].ToString(), 0);
+                    }
+                    else
+                    {
+                        UpdateTokenPrice(rdr[0].ToString(), token_price);
+                    }
                 }
             }
             catch (Exception e)
@@ -80,13 +80,44 @@ namespace EtherscanConsole
             Console.ReadKey();
         }
 
+
+        public static void UpdateTokenPrice(string symbol, decimal price)
+        {
+            string token = "";
+            try
+            {
+                var con = ConfigurationManager.ConnectionStrings["mysqlConnection"].ConnectionString;
+                MySqlConnection conn = new MySqlConnection(con);
+                conn.Open();
+
+                var query = "Update token SET price=@price WHERE symbol=@symbol";
+                MySqlCommand cmd = new MySqlCommand(query, conn);
+                cmd.Parameters.AddWithValue("@symbol", symbol);
+                cmd.Parameters.AddWithValue("@price", price);
+                cmd.ExecuteNonQuery();
+
+                cmd.Dispose();
+                //dr.Dispose();
+                conn.Dispose();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
         //Token Model
         public class Token
         {
             public string symbol { get; set; }
             public int price { get; set; }
         }
-
+        //Results from API
+        public class TokenPrice
+        {
+            public string USD { get; set; }
+            public string Response { get; set; }
+            public string Message { get; set; }
+        }
         //Calling the API with GET request for respective token price
         public static string GetTokenPrice(string url)
         {
@@ -98,11 +129,10 @@ namespace EtherscanConsole
             HttpWebResponse res = (HttpWebResponse)req.GetResponse();
             Encoding encoding = System.Text.Encoding.GetEncoding("utf-8");
             StreamReader sr = new StreamReader(res.GetResponseStream(), encoding);
-            string result = string.Empty;
-            result = sr.ReadToEnd();
+            string json = sr.ReadToEnd();
             res.Close();
 
-            return result;
+            return json;
         }
     }
 }
